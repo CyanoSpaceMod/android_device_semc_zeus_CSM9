a7a5329de44b39767da92e988f2adebbe9c5e796
spacecaker <robin-barry@hotmail.com>
2013-05-14 16:10:20 +0200

    Upload CSM XPLAY device source

---
+++ BoardConfig.mk
@@ -0,0 +1,20 @@
+-include device/semc/msm7x30-common/BoardConfigCommon.mk
+-include device/semc/zeus-common/BoardConfigCommon.mk
+-include vendor/semc/zeus/BoardConfigVendor.mk
+
+TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
+TARGET_KERNEL_CONFIG := cyanogen_zeus_defconfig
+TARGET_PREBUILT_KERNEL := device/semc/zeus/prebuilt/kernel
+
+SENSORS_COMPASS_AK8975 := true
+SENSORS_ACCEL_BMA150_INPUT := true
+SENSORS_ACCEL_BMA250_INPUT := false
+SENSORS_PROXIMITY_SHARP_GP2 := true
+SENSORS_PRESSURE_BMP180 := false
+
+TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
+
+BOARD_USES_STEREO_HW_SPEAKER := true
+
+TARGET_OTA_ASSERT_DEVICE := R800i,R800a,R800at,zeus
+
---
+++ device_zeus.mk
@@ -0,0 +1,45 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# common msm7x30 configs
+$(call inherit-product, device/semc/msm7x30-common/msm7x30.mk)
+
+# common mogami configs
+$(call inherit-product, device/semc/zeus-common/zeus.mk)
+
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/semc/zeus/zeus-vendor.mk)
+
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
+DEVICE_PACKAGE_OVERLAYS += device/semc/zeus/overlay
+
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES += \
+    device/semc/zeus/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+
+# Copy bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/semc/zeus/bootanim/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/zeus/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
+    device/semc/zeus/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    device/semc/zeus/recovery.fstab:root/recovery.fstab \
+    device/semc/zeus/prebuilt/bootrec-device:root/sbin/bootrec-device
+
+$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
+
+#LCD DENSITY
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    com.qc.hdmi_out=false
+    ro.space.version=CyanoSpaceMod RC2
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
